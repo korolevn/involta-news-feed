41dc43db7482760a457e172730b8e23d2f2085eb
nkorolev <orevdrol@mail.ru>
2024-10-31 01:36:42 +0300

    feat(assets): add favicon

--- app.vue
+++ app.vue
@@ -9,6 +9,7 @@
 <script setup lang="ts">
 useHead({
     title: "Список новостей",
+    link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     bodyAttrs: {
         class: "bg-main",
     },
