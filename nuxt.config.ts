391dd16c43c246b29010e2e2fdaccf6a665c0b4f
nkorolev <orevdrol@mail.ru>
2024-10-30 23:01:26 +0300

    feat: add project initial state

---
+++ nuxt.config.ts
@@ -0,0 +1,14 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "@vueuse/nuxt",
+    "@nuxt/fonts",
+    '@pinia/nuxt',
+  ],
+  devServer: {
+    port: 8000
+  },
+})
